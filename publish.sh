5550a5c39e4d8ab887a0be7b326e958542cb9728
manashjyotibarman2004-pixel <manashjyotibarman2004@gmail.com>
2026-06-13 03:18:02 +0530

    added extra jsn and html

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+git add .
+echo "Enter commit message:"
+read msg
+git commit -m "$msg"
+git push origin main
+
+echo ""
+echo "✅ Website published successfully!"
+
+
